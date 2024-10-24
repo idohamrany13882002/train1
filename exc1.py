29920a97ea25fd7a031e670ed69bb2e807bc35c2
idohamrany <idohamrany13882002@gmail.com>
2024-10-24 11:10:06 +0300

    first upload

---
+++ exc1.py
@@ -0,0 +1,3 @@
+num: float = float(input("Enter a number: "))
+
+print (num-10) if num >10 else print (num*10)
---
+++ exc2.py
@@ -0,0 +1,5 @@
+num1: float = float(input("Enter a number: "))
+num2: float = float(input("Enter a number: "))
+num3: float = float(input("Enter a number: "))
+
+print(num1 + num2 + num3) if num1 + num2 + num3 > 100 else print("Sum is smaller than 100")
